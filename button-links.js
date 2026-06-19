a7f2ed8946330dff0664422685c601b32f988367
Adrian Alx <49885385+AdrianEnigma@users.noreply.github.com>
2026-06-18 21:34:08 -0300

    ..

---
+++ button-links.js
@@ -0,0 +1,5 @@
+// Edite estes dois links quando quiser apontar os botoes do Plano Basico e Premium para o checkout.
+window.BUTTON_LINKS = {
+  checkoutBasic: "https://www.basic.com/",
+  checkoutPremium: "https://www.premium.com"
+};
