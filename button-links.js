d56e6e88f807614bb1fdb33d1d0de3dd46882346
Adrian Alx <49885385+AdrianEnigma@users.noreply.github.com>
2026-06-22 22:19:08 -0300

    dasa

--- button-links.js
+++ button-links.js
@@ -1,5 +1,5 @@
 // Edite estes dois links quando quiser apontar os botoes do Plano Basico e Premium para o checkout.
 window.BUTTON_LINKS = {
-  checkoutBasic: "https://ggcheckout.app/checkout/v4/CApHxhPwzfWlbnidt4tN",
-  checkoutPremium: "https://ggcheckout.app/checkout/v4/AnNzKZIhzOePDbVFqBNs"
+  checkoutBasic: "https://checkout.wiven.com.br/checkout/cmql8akrm08wk01q1lo9i4zjo?offer=KOKU1EG",
+  checkoutPremium: "https://checkout.wiven.com.br/checkout/cmql8akrm08wk01q1lo9i4zjo?offer=YQ9F49Z"
 };
