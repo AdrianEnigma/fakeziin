ab49b2d5693e193f6c5abec593551816a0541cac
Adrian Alx <49885385+AdrianEnigma@users.noreply.github.com>
2026-07-23 21:01:31 -0300

    Update links.js

--- links.js
+++ links.js
@@ -2,9 +2,9 @@ window.FAKEZIN_LINKS = {
   email: "contatofakesera@gmail.com",
 
   youtube: "https://www.youtube.com/@fakezintech",
-  tiktok: "#",
+  tiktok: "https://www.tiktok.com/@fakezintech?is_from_webapp=1&sender_device=pc",
   twitch: "#",
-  instagram: "#",
+  instagram: "https://www.instagram.com/fakezintech?igsh=NjBzcjB0ZGNpdGp4&utm_source=qr",
 
   produtos: "#",
   livePix: "#",
